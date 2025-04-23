6a7f1dc7965419f425f0132411f9a3fcc23c3d4d
SabarishCodeWizard <ravik60656@gmail.com>
2025-04-23 21:21:56 +0530

    rollback the  Login / Google Sign-In

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -10,7 +10,13 @@ const HomePage = () => {
                 <p>Effortlessly discover and manage all campus events — whether you're organizing or attending.</p>
             </div>
 
-
+            <div className="right-section">
+                <h2>Login</h2>
+                <div className="button-container">
+                    <Link to="/admin" className="btn admin-btn">Admin Login</Link>
+                    <Link to="/student-login" className="btn student-btn">Student Login</Link>
+                </div>
+            </div>
         </div>
     );
 };
