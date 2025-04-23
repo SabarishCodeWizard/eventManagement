729794f3aa90641ff7eefe850451abc0dd65a274
SabarishCodeWizard <ravik60656@gmail.com>
2025-04-23 21:21:20 +0530

    Loading Spinner on Login / Google Sign-In

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -10,13 +10,7 @@ const HomePage = () => {
                 <p>Effortlessly discover and manage all campus events — whether you're organizing or attending.</p>
             </div>
 
-            <div className="right-section">
-                <h2>Login</h2>
-                <div className="button-container">
-                    <Link to="/admin" className="btn admin-btn">Admin Login</Link>
-                    <Link to="/student-login" className="btn student-btn">Student Login</Link>
-                </div>
-            </div>
+
         </div>
     );
 };
